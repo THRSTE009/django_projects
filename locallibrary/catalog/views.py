40aea0ed7f6c9743975b7bd5c67b06f24419fa44
Steven Theron <steve.theron365@gmail.com>
2022-06-02 12:32:56 +0200

    Mozilla Django - Tutorial part 6
    Generic list and detail views.
    
    Remaining challenge:
    Display book copies on author_detail template. /catalog/author/1

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-from .models import Book, Author, BookInstance, Genre
+from catalog.models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 
 def index(request):
@@ -28,3 +29,59 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+
+# ListView is a class-based generic list view that inherits from an existing view which follows Django best-practice.
+#   Making it more robust, less code, less repetition and less maintenance than standard views.
+class BookListView(generic.ListView):
+    """This generic view queryies the db to get all records for the specified model (Book) then it renders a template."""
+    # Template location: /locallibrary/catalog/templates/catalog/book_list.html
+    # Within the template you can access the list of books with the template variable named "object_list" OR "book_list"
+    # (i.e. generically "<the model name>_list").
+
+    model = Book
+    paginate_by = 2
+
+    # context_object_name = 'book_list' # custom name for the list as a template variable.
+        # context passed by default as "object_list" or "book_list".
+    def get_context_data(self, **kwargs):
+        #1. Call the base implementation first to get the context.
+        context = super(BookListView, self).get_context_data(**kwargs)
+        #2. Create any data and add it to the context.
+        context['some_data'] = 'This is just some data'
+        return context #3. Return new (updated) context.
+
+    # Option 1:
+        # queryset = Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title 'war'.
+    # Option 2:
+    # def get_queryset(self):
+    #     return Book.objects.filter(title__icontains='war')[:5]
+
+    template_name = 'books/my_arbitray_template_name_list.html' # Specify your own template name/location.
+
+
+
+# def book_detail_view(request, primary_key):   *CLASS-BASED ALTERNATIVE FOR THE DETAIL VIEW*
+#     book = get_object_or_404(Book, pk=primary_key)
+#     return render(request, 'catalog/book_detail.html', context={'book': book})
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+
+class AuthorListView(generic.ListView):
+    """This generic view queryies the db to get all records for Author then it renders a template."""
+    model = Author
+    paginate_by = 10
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+
+    # num_instances = BookInstance.objects.filter(__str__=author).count()
+    #
+    # def get_context_data(self, **kwargs):
+    #     context = super(AuthorListView, self).get_context_data(**kwargs)
+    #     context['num_instances'] = 'num_instances'
+    #     return context
